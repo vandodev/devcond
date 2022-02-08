67031bbe127457bec62c5c5f262313e9a111d368
Evandro oliveira <vandoaparecido@hotmail.com>
2022-02-08 00:12:23 -0300

    :calendar: Criando o context

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {StateProvider} from './src/contexts/StateContext';
 
 export default () => {
   return (
-    <View>
-      <Text>Funcionou</Text>
-    </View>
+    <StateProvider>
+      <View>
+        <Text>Funcionou</Text>
+      </View>
+    </StateProvider>
   );
 };
