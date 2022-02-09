8f8f1366ae77d93a10d8d4f2eca41c2dac71dc48
Evandro oliveira <vandoaparecido@hotmail.com>
2022-02-09 01:03:15 -0300

    :calendar: Criando e estilizando campos da tela de login

--- mobile/src/screens/LoginScreen/index.js
+++ mobile/src/screens/LoginScreen/index.js
@@ -9,9 +9,37 @@ export default () => {
   const navigation = useNavigation();
   const [context, dispatch] = useStateValue();
 
+  const [cpf, setCpf] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <C.Container>
-      <C.Texto>Tela de login</C.Texto>
+      <C.Logo
+        source={require('../../assets/undraw_home.png')}
+        resizeMode="contain"
+      />
+
+      <C.Field
+        placeholder="Digite seu CPF"
+        keyboardType="numeric"
+        value={cpf}
+        onChangeText={t => setCpf(t)}
+      />
+
+      <C.Field
+        placeholder="Digite sua senha"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={t => setPassword(t)}
+      />
+
+      <C.ButtonArea onPress={null}>
+        <C.ButtonText>ENTRAR</C.ButtonText>
+      </C.ButtonArea>
+
+      <C.ButtonArea onPress={null}>
+        <C.ButtonText>CADASTRAR-SE</C.ButtonText>
+      </C.ButtonArea>
     </C.Container>
   );
 };
--- mobile/src/screens/LoginScreen/style.js
+++ mobile/src/screens/LoginScreen/style.js
@@ -3,10 +3,37 @@ import styled from 'styled-components/native';
 export default {
   Container: styled.SafeAreaView`
     flex: 1;
+    padding: 20px;
+    background-color: #f5f6fa;
+  `,
+  Logo: styled.Image`
+    width: 250px;
+    height: 200px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 20px;
+  `,
+  Field: styled.TextInput`
+    border-width: 1px;
+    border-color: #ccc;
+    background-color: #fff;
+    border-radius: 5px;
+    color: #000;
+    font-size: 15px;
+    padding: 10px;
+    margin-bottom: 15px;
+  `,
+  ButtonArea: styled.TouchableOpacity`
+    background-color: #8863e6;
+    padding: 12px;
     justify-content: center;
     align-items: center;
+    border-radius: 5px;
+    margin-bottom: 15px;
   `,
-  Texto: styled.Text`
-  
-  `
+  ButtonText: styled.Text`
+    color: #fff;
+    font-size: 15px;
+    font-weight: bold;
+  `,
 };
