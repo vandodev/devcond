0480e917b33149afef45aeb595abd1c621357bcd
Evandro oliveira <vandoaparecido@hotmail.com>
2022-02-13 00:30:19 -0300

    :calendar: Configurando menu hamburger

--- mobile/src/stacks/MainDrawer.js
+++ mobile/src/stacks/MainDrawer.js
@@ -8,7 +8,17 @@ import WallScreen from '../screens/WallScreen';
 
 export default () => {
   return (
-    <Drawer.Navigator drawerContent={props => <DrawerCustom {...props} />}>
+    <Drawer.Navigator
+      drawerContent={props => <DrawerCustom {...props} />}
+      screenOptions={{
+        headerShown: true,
+        headerTitle: '',
+        headerStyle: {
+          backgroundColor: '#F5F6FA',
+          shadowOpacity: 0,
+          elevation: 0,
+        },
+      }}>
       <Drawer.Screen name="WallScreen" component={WallScreen} />
     </Drawer.Navigator>
   );
