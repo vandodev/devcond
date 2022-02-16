13bc9d3ee2a87c9feaecdca01ea6e1206f590c61
Evandro oliveira <vandoaparecido@hotmail.com>
2022-02-16 00:21:37 -0300

    :calendar: Estilizando ocorrências

--- mobile/src/components/WarningItem.js
+++ mobile/src/components/WarningItem.js
@@ -1,38 +1,47 @@
 import React, {useState} from 'react';
-import {Linking} from 'react-native';
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import api from '../services/api';
 
-const Box = styled.TouchableOpacity`
+const Box = styled.View`
   background-color: #fff;
   border-width: 2px;
   border-color: #e8e9ed;
   border-radius: 15px;
   padding: 15px;
   margin-bottom: 10px;
-  flex-direction: row;
-  align-items: center;
+`;
+const Date = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #9c9db9;
+  margin-bottom: 10px;
 `;
 const Title = styled.Text`
   font-size: 15px;
   color: #000;
+`;
+const StatusArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 10px 0;
+`;
+const StatusText = styled.Text`
+  font-size: 14px;
+  color: #9c9db9;
   margin-left: 10px;
 `;
 
 export default ({data}) => {
-  const handleClick = async () => {
-    const supported = await Linking.canOpenURL(data.fileurl);
-    if (supported) {
-      await Linking.openURL(data.fileurl);
-    }
-  };
-
   return (
-    <Box onPress={handleClick}>
-      <Icon name="file-text" size={30} color="#8B63E7" />
+    <Box>
+      <Date>{data.datecreated}</Date>
       <Title>{data.title}</Title>
+      <StatusArea>
+        <Icon name="inbox" size={24} color="#8B63E7" />
+        <StatusText>{data.status} </StatusText>
+      </StatusArea>
     </Box>
   );
 };
