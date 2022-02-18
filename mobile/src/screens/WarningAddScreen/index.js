a9c7a67a099bfe07009a9496c66d7b446b5a5459
Evandro oliveira <vandoaparecido@hotmail.com>
2022-02-18 00:31:03 -0300

    :calendar: Estilizando componentes

--- mobile/src/screens/WarningAddScreen/index.js
+++ mobile/src/screens/WarningAddScreen/index.js
@@ -10,11 +10,35 @@ export default () => {
   const navigation = useNavigation();
   const [context, dispatch] = useStateValue();
 
+  const [warnText, setWarnText] = useState('');
+
   useEffect(() => {
     navigation.setOptions({
       headerTitle: 'Adicionar uma Ocorrências',
     });
   }, []);
 
-  return <C.Container></C.Container>;
+  return (
+    <C.Container>
+      <C.Scroller>
+        <C.Title>Descreva a ocorrência</C.Title>
+        <C.Field
+          placeholder="Ex: Vizinho X está com som alto"
+          value={warnText}
+          onChangeText={t => setWarnText(t)}
+        />
+        <C.Title>Fotos relacionadas</C.Title>
+        <C.PhotoArea>
+          <C.PhotoScroll horizontal>
+            <C.PhotoAddButton onPress={null}>
+              <Icon name="camera" size={24} color="#000" />
+            </C.PhotoAddButton>
+          </C.PhotoScroll>
+        </C.PhotoArea>
+        <C.ButtonArea onPress={null}>
+          <C.ButtonText>Salvar</C.ButtonText>
+        </C.ButtonArea>
+      </C.Scroller>
+    </C.Container>
+  );
 };
